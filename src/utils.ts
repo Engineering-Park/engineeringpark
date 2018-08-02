379e4dc244a9fd43b5fbc9ffe9da89978199a10c
Tristan Martindale <tristanmartindale@gmail.com>
2018-08-02 22:36:15 +0200

    Added distance between two points utility function.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
+import { Vector3Component } from 'metaverse-api'
+
 export function sleep(ms: number = 0) {
   return new Promise(r => setTimeout(r, ms));
+}
+
+export function distanceBetweenTwoPoints(p1: Vector3Component, p2: Vector3Component) {
+  const dx = p1.x - p2.x;
+  const dy = p1.y - p2.y;
+  const dz = p1.z - p2.z;
+
+  const dxy = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+  const dxz = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
+
+  return Math.sqrt(Math.pow(dxy, 2) + Math.pow(dxz, 2));
 }
