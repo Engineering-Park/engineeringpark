b704c46188f58f06503feacd1583433053e12607
Tristan Martindale <tristanmartindale@gmail.com>
2018-08-12 10:58:03 +0200

    [major] Removed simonsays and rollercoaster from the scene.

--- scene.tsx
+++ scene.tsx
@@ -1,8 +1,9 @@
-import { createElement, ISimplifiedNode, ScriptableScene } from 'metaverse-api'
-import { ComposeableScene } from './src/composeablescene'
-import RollerCoaster from "./src/components/RollerCoaster";
-import SimonSays from "./src/components/SimonSays";
+import { createElement, ScriptableScene } from 'metaverse-api'
 import { Pedestal } from "./src/components/Pedestal";
+import { createStore } from 'redux'
+import { rootReducer } from './src/store'
+
+const store = createStore(rootReducer);
 
 const colors = ['#3d9693', '#e8daa0', '#968fb7', '#966161', '#879e91', '#66656b', '#6699cc'];
 
@@ -12,13 +13,7 @@ interface State {
     donutAngle: number;
 }
 
-interface ComposeableSceneContainer {
-  [index: string]: ComposeableScene<any, any>
-}
-
 export default class OSEVRScene extends ScriptableScene<any, State> {
-  private components: ComposeableSceneContainer;
-
   constructor(props: any) {
     super(props);
     this.state = {
@@ -26,17 +21,6 @@ export default class OSEVRScene extends ScriptableScene<any, State> {
       dogAngle: 0,
       donutAngle: 0
     };
-    this.components = {};
-
-    this.components['RollerCoaster'] = new RollerCoaster({
-      position: { x: 30, y: 4, z: 0 },
-      rotation: { x: 0, y: 0, z: 0 }
-    });
-
-    this.components['SimonSays'] = new SimonSays({
-      position: { x: -10, y: 0, z: 0 },
-      rotation: { x: 0, y: 180, z: 0 }
-    });
   }
 
   public async sceneDidMount() {
@@ -59,7 +43,6 @@ export default class OSEVRScene extends ScriptableScene<any, State> {
   public async render() {
     return (
       <scene position={{ x: 5, y: 0, z: 5 }}>
-        {this.renderComponents()}
         <Pedestal
           id='pedestal'
           position={{x:20, y:0.5, z:0}}
@@ -103,17 +86,5 @@ export default class OSEVRScene extends ScriptableScene<any, State> {
         />
       </scene>
     );
-  }
-
-  private renderComponents() {
-    let renderedComponents: Array<ISimplifiedNode>=[];
-
-    for(var key in this.components) {
-      if(this.components.hasOwnProperty(key)) {
-        renderedComponents.push(this.components[key].render());
-      }
-    }
-
-    return renderedComponents;
   }
 }
