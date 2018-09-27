3dd04a54ce979debf3e14f9baedeadd645fee650
Tristan Martindale <tristanmartindale@gmail.com>
2018-09-27 23:44:48 +0200

    [tweak] Replaced pedestal with gltf model.

--- src/components/Pedestal.tsx
+++ src/components/Pedestal.tsx
@@ -10,17 +10,8 @@ export interface Props {
 export const Pedestal = (props: Props) => {
   return (
     <entity position={props.position}>
-      <box
-        id={props.id}
-        color={props.color}
-        position={{ x: 0, y: 0.3, z: 0 }}
-        scale={{ x: 0.45, y: 0.7, z: 0.45 }}
-      />
-      <box
-        id={props.id}
-        position={{ x: 0, y: 0.7, z: 0 }}
-        color={props.color}
-        scale={{ x: 0.55, y: 0.20, z: 0.55 }}
+      <gltf-model
+        src='assets/models/pedestal.gltf'
       />
       <text
         value={props.value}
