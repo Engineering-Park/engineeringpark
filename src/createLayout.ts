d9277b429433595730e7657b1baa860f3f85a726
Tristan Martindale <tristanmartindale@gmail.com>
2021-01-16 17:48:20 +0100

    [tweak] fix sbs layout

--- src/createLayout.ts
+++ src/createLayout.ts
@@ -44,8 +44,8 @@ export default function createLayout() {
   return {
     shapes: shapes(),
     connectors: connectors(),
-    width: 26,
-    height: 14,
+    width: 13.5,
+    height: 11,
     orientation: Orientation.vertical
   };
 }
@@ -53,72 +53,72 @@ export default function createLayout() {
 const shapes = () => [
   {
     name: "Data",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 1, y: 13 },
+    position: { x: 1.5, y: 10 },
     rotation: 0
   },
   {
     name: "Hardware",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 5, y: 13 },
+    position: { x: 5, y: 10 },
     rotation: 0
   },
   {
     name: "Subsystem",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 3, y: 9 },
+    position: { x: 5, y: 7 },
     rotation: 0
   },
   {
     name: "System",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 6, y: 5 },
+    position: { x: 5, y: 4 },
     rotation: 0
   },
   {
     name: "Software",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 17, y: 13 },
+    position: { x: 8.5, y: 10 },
     rotation: 0
   },
   {
     name: "Service",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 19, y: 9 },
+    position: { x: 12, y: 7 },
     rotation: 0
   },
   {
     name: "System of Systems",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 22, y: 5 },
+    position: { x: 12, y: 4 },
     rotation: 0
   },
   {
     name: "System of Interest",
-    width: 2,
+    width: 3,
     height: 2,
-    position: { x: 14, y: 1 },
+    position: { x: 8.5, y: 1 },
     rotation: 0
   }
 ];
 
 const connectors = () => [
-  { p1: { x: 3, y: 9 }, p2: { x: 1, y: 13 } }, // element 3 to element 1
-  { p1: { x: 3, y: 9 }, p2: { x: 5, y: 13 } }, // element 3 to element 2
+  { p1: { x: 3, y: 9 }, p2: { x: 1, y: 12 } }, // element 3 to element 1
+  { p1: { x: 3, y: 9 }, p2: { x: 5, y: 12 } }, // element 3 to element 2
   { p1: { x: 6, y: 5 }, p2: { x: 3, y: 9 } }, // element 5 to element 3
   { p1: { x: 6, y: 5 }, p2: { x: 9, y: 9 } }, // element 5 to element 4
-  { p1: { x: 19, y: 9 }, p2: { x: 17, y: 13 } }, // element 9 to element 7
-  { p1: { x: 19, y: 9 }, p2: { x: 21, y: 13 } }, // element 9 to element 8
+  { p1: { x: 19, y: 9 }, p2: { x: 17, y: 12 } }, // element 9 to element 7
+  { p1: { x: 19, y: 9 }, p2: { x: 21, y: 12 } }, // element 9 to element 8
   { p1: { x: 22, y: 5 }, p2: { x: 19, y: 9 } }, // element 11 to element 9
   { p1: { x: 22, y: 5 }, p2: { x: 25, y: 9 } }, // element 11 to element 10
   { p1: { x: 14, y: 1 }, p2: { x: 6, y: 5 } }, // element 12 to element 5
-  { p1: { x: 14, y: 1 }, p2: { x: 13, y: 5 } }, // element 12 to element 6
+  { p1: { x: 14, y: 1 }, p2: { x: 12, y: 5 } }, // element 12 to element 6
   { p1: { x: 14, y: 1 }, p2: { x: 22, y: 5 } } // element 12 to element 11
 ];
